3a07dc3041694daffaa40653f98d35d9612c9c05
Your Name <you@example.com>
2022-09-23 16:12:30 +0700

    update1

--- listed/index.c
+++ listed/index.c
@@ -4,50 +4,57 @@ int main()
 {
     // nhap vao n
     int n;
-    printf("nhap vao n: ");
+    printf("Nhap vao n: ");
     scanf("%d", &n);
-    printf("\n Cac bo hoan vi la: \n");
-    // khoi tao mang gom n phan tu
-    char mang[n];
-    // gan gia tri cho cac phan tu cua mang va in ra bo hoan vi dau tien
+
+    printf("\n Cac bo hoan vi la:\n");
+
+    // khoi tao arr gom n phan tu
+    char arr[n];
+
+    // gan gia tri cho cac phan tu cua arr va in ra bo hoan vi dau tien
     int i;
     for (i = 0; i < n; i++)
     {
-        mang[i] = i + 65;
-        printf("%d", mang[i]);
+        arr[i] = i + 65;
+        printf("%d", arr[i]);
     }
     // xu ly de in ra cac bo hoan vi tiep theo
     for (i = n - 1; i > 0; i--)
     {
         int j;
-        //neu gap mang[i] > mang[i - 1]
-        if (mang[i] > mang[i - 1])
+        // neu gap arr[i] > arr[i - 1]
+        if (arr[i] > arr[i - 1])
         {
-            
-            //tim trong bo cuoi giam dan phan tu vua du lon hon mang[i - 1] sau do swap
+
+            // tim trong bo cuoi giam dan phan tu vua du lon hon mang[i - 1] sau do swap
             for (j = n - 1; j >= i; j--)
             {
-                int temp = mang[j];
-                mang[j] = mang[i - 1];
-                mang[i - 1] = temp;
-                break;
+                if (arr[j] > arr[i - 1])
+                {
+                    int temp = arr[j];
+                    arr[j] = arr[i - 1];
+                    arr[i - 1] = temp;
+                    break;
+                }
             }
         }
-        //dao nguoc bo cuoi
-        for (j = n - 1; j > ((n - 1 + i)/2); j--) {
-            int temp = mang[i + n - 1 - j];
-            mang[i+n - 1-j] = mang[j];
-            mang[j] = temp;
+        // dao nguoc bo cuoi
+        for (j = n - 1; j > ((n - 1 + i) / 2); j--)
+        {
+            int temp = arr[i + n - 1 - j];
+            arr[i + n - 1 - j] = arr[j];
+            arr[j] = temp;
         }
-        //in ra cac bo hoan vi moi
+        // in ra cac bo hoan vi moi
         printf("\n");
-        for (j = 0; j < n; j++) { 
-            printf("%d", mang[j]);
+        for (j = 0; j < n; j++)
+        {
+            printf("%d", arr[j]);
         }
-        //i = n de khi thoat vong lap hien tai i-- thanh i = n - 1; va vong lap 
+        // i = n de khi thoat vong lap hien tai i-- thanh i = n - 1; va vong lap
         i = n;
     }
     getch();
     return 0;
-    
 }
